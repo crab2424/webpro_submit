0e97f5d0ea374a046c10f944efbaa382b6c6fa3d
crab2424 <crabipad2424@gmail.com>
2025-12-25 11:26:47 +0900

    change

--- app/app_system.js
+++ app/app_system.js
@@ -104,10 +104,15 @@ app.get("/pref/delete/:id", (req, res) => {
 
 //データ
 let constellations = [
-    { id: 0, name: "星座" ,code: 0, area: 0, population: 0, capital: "", region: "" },
-    { id: 1, name: "東京都" ,code: 13, area: 2199.94, population: 14273066, capital: "新宿区", region: "関東" },
-    { id: 2, name: "大阪府" ,code: 27, area: 1905.25, population: 8777998, capital: "大阪市", region: "近畿" },
-    { id: 3, name: "福岡県" ,code: 40, area: 4987.66, population: 5088841, capital: "福岡市", region: "九州" },
+    { id: 0, name: "星座" , code: "", shape: "", height: "", star: "", season: "" },
+    { id: 1, name: "オリオン座" , en: "Orion", shape: "男性", height: "57", star: "ベテルギウス", season: "冬" },
+    { id: 2, name: "かに座" , en: "Cancer", shape: "動物", height: "74", star: "アクベンス", season: "春" },
+    { id: 3, name: "カシオペヤ座" , en: "Cassiopeia", shape: "女性", height: "N66", star: "シェダル", season: "秋" },
+    { id: 4, name: "はくちょう座" , en: "Cygnus", shape: "動物", height: "N83", star: "デネブ", season: "夏" },
+    { id: 5, name: "さそり座" , en: "Scorpius", shape: "動物", height: "28", star: "アンタレス", season: "夏" },
+    { id: 6, name: "こぐま座" , en: "Ursa minor", shape: "動物", height: "N48", star: "北極星", season: "春" },
+    { id: 7, name: "ケンタウルス座" , en: "Centaurus", shape: "空想動物", height: "8", star: "リギル・ケンタウルス", season: "南天" },
+    { id: 8, name: "こと座" , en: "Lyra", shape: "道具", height: "90", star: "ベガ", season: "夏"}
 ];
 
 //一覧
@@ -117,7 +122,7 @@ app.get("/stella", (req, res) => {
 
 //新規フォーム
 app.get("/stella/create", (req, res) => {
-    res.redirect('/public/pref_new.html');
+    res.redirect('/public/stella_new.html');
 });
 
 
@@ -146,12 +151,12 @@ app.get("/stella/check/:id", (req, res) => {
 app.post("/stella", (req, res) => {
     const id = constellations.length;
     const name = req.body.name;
-    const code = req.body.code;
-    const area = req.body.area;
-    const population = req.body.population;
-    const capital = req.body.capital;
-    const region = req.body.region;
-    constellations.push({ id: id, name: name, code: code, area: area, population: population, capital: capital, region: region });
+    const en = req.body.en;
+    const shape = req.body.shape;
+    const height = req.body.height;
+    const star = req.body.star;
+    const season = req.body.season;
+    constellations.push({ id: id, name: name, en: en, shape: shape, height: height, star: star, season: season });
     console.log('新規追加後の星座：', req.body);
 
     if (req.body.page === '登録して新たに作成') {
@@ -164,11 +169,11 @@ app.post("/stella", (req, res) => {
 //更新処理
 app.post("/stella/update/:id", (req, res) => {
     constellations[req.params.id].name = req.body.name;
-    constellations[req.params.id].code = req.body.code;
-    constellations[req.params.id].area = req.body.area;
-    constellations[req.params.id].population = req.body.population;
-    constellations[req.params.id].capital = req.body.capital;
-    constellations[req.params.id].region = req.body.region;
+    constellations[req.params.id].en = req.body.en;
+    constellations[req.params.id].shape = req.body.shape;
+    constellations[req.params.id].height = req.body.height;
+    constellations[req.params.id].star = req.body.star;
+    constellations[req.params.id].season = req.body.season;
     console.log('更新後の星座：', constellations[req.params.id]);
     res.render('stella/stella_detail', {id: req.params.id, data: constellations[req.params.id]} );
 });
